1709576ed7b3ed75b5eb2d2a7c9533cf469a0bab
cryosx <cyruswu.email@gmail.com>
2018-04-23 11:07:38 -1000

    adjusted translator to pig latin requirements

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -4,14 +4,21 @@ function pigLatinTranslator(text) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   let targetString = text;
   return {
+    getText: getText,
     setText: setText,
     getPigLatin: translateSentenceInto,
     getEnglish: translateSentenceOut
   };
 
+  function getText() {
+    return targetString;
+  }
+
   function setText(text) {
     if (typeof text === 'string') {
       targetString = text;
+    } else {
+      throw new Error('Not a string');
     }
   }
 
@@ -31,28 +38,44 @@ function pigLatinTranslator(text) {
     return result.join(' ');
   }
 
+  function getSplitIndex(string) {
+    let index = null;
+    for (let i = 0; i < string.length; i++) {
+      if (vowels.includes(string[i])) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+
   function translateWordInto(string) {
     let firstLetter = string[0].toLowerCase();
+    let splitIndex = getSplitIndex(string);
+    let firstHalf = string.toLowerCase().slice(0, splitIndex);
+    let secondHalf = string.slice(splitIndex);
+
     let translatedString = null;
     if (vowels.includes(firstLetter)) {
       translatedString = `${string}-ay`;
     } else {
-      translatedString = `${string.slice(1)}-${firstLetter}ay`;
+      translatedString = `${secondHalf}-${firstHalf}ay`;
     }
     return translatedString;
   }
   function translateWordOut(string) {
+    if (!string.includes('-')) {
+      return string;
+    }
+    let targetString = string.slice(0, string.length - 2);
     let firstLetter = string[0].toLowerCase();
     let translatedString = null;
-    let stringSplit = string.split('-');
-    if (vowels.includes(firstLetter)) {
-      translatedString = stringSplit[0];
+    let stringSplit = targetString.split('-');
+    if (stringSplit[1]) {
+      console.log(stringSplit[1]);
+      translatedString = `${stringSplit[1]}${stringSplit[0]}`;
     } else {
-      if (stringSplit[1]) {
-        translatedString = `${stringSplit[1][0]}${stringSplit[0]}`;
-      } else {
-        translatedString = stringSplit[0];
-      }
+      translatedString = stringSplit[0];
     }
     return translatedString;
   }
--- test/pig-latin-translator-test.js
+++ test/pig-latin-translator-test.js
@@ -7,18 +7,55 @@ describe('pigLatinTranslator', function() {
   beforeEach(function() {
     translator = pigLatinTranslator('test');
   });
+  describe('getText()', function() {
+    // translator = pigLatinTranslator('test');
+    it('should be a function', function() {
+      expect(translator).to.respondTo('getText');
+    });
+    it(`should be able to get the string that will be translated into/out of pig latin`, function() {
+      expect(translator.getText()).to.equal('test');
+    });
+  });
+  describe('setText()', function() {
+    // translator = pigLatinTranslator('test');
+    it('should be a function', function() {
+      expect(translator).to.respondTo('setText');
+    });
+    it(`should be able to set the string to be translated, 'test' => 'apple`, function() {
+      expect(translator.getText()).to.equal('test');
+      translator.setText('apple');
+      expect(translator.getText()).to.equal('apple');
+    });
+  });
   describe('getPigLatin()', function() {
     // translator = pigLatinTranslator('test');
     it('should be a function', function() {
       expect(translator).to.respondTo('getPigLatin');
     });
-    it('should translate into pig latin', function() {
+    it(`should translate words into pig latin, 'test' => 'est-tay`, function() {
       expect(translator.getPigLatin()).to.equal('est-tay');
+    });
+    it(`should translate sentences into pig latin, 'test test' => 'est-tay esy-tay`, function() {
+      translator.setText('test test');
+      expect(translator.getPigLatin()).to.equal('est-tay est-tay');
+    });
+    it(`should translate sentences into pig latin, 'Good morning everyone' => 'ood-gay orning-may everyone-ay'`, function() {
+      translator.setText('Good morning everyone');
+      expect(translator.getPigLatin()).to.equal(
+        'ood-gay orning-may everyone-ay'
+      );
     });
   });
   describe('getEnglish()', function() {
     it('should be a function', function() {
       expect(translator).to.respondTo('getPigLatin');
+    });
+    it(`should translate into pig latin, 'test' => 'test'`, function() {
+      expect(translator.getEnglish()).to.equal('test');
+    });
+    it(`should translate into pig latin, 'est-tay' => 'test`, function() {
+      translator.setText('est-tay');
+      expect(translator.getEnglish()).to.equal('test');
     });
   });
 });
